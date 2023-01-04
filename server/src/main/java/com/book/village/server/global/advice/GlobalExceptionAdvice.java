ce14ac01e6ccefc9e6cfa34032ffbc468d8dee44
orioncsy <orioncsy@gmail.com>
2023-01-04 20:19:53 +0900

    init : create server & client

---
+++ server/src/main/java/com/book/village/server/global/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,34 @@
+package com.book.village.server.global.advice;
+
+import com.book.village.server.global.exception.CustomLogicException;
+import com.book.village.server.global.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+
+        return ErrorResponse.of(e.getBindingResult());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(
+            ConstraintViolationException e) {
+        return ErrorResponse.of(e.getConstraintViolations());
+    }
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomLogicException e) {
+        return new ResponseEntity<>(ErrorResponse.of(e), HttpStatus.valueOf(e.getExceptionCode().getCode()));
+    }
+}
---
+++ server/src/main/java/com/book/village/server/global/response/SingleResponse.java
@@ -0,0 +1,10 @@
+package com.book.village.server.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SingleResponse<T> {
+    private T data;
+}
