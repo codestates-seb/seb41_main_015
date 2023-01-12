78ff24486417ec5adeb9636aaba341fbe4e746d5
aemaaeng <haemin120@naver.com>
2023-01-12 16:22:54 +0900

    design/fe/reqDetail: 요청 상세 페이지 디자인

--- client/src/page/ReqDetail.js
+++ client/src/page/ReqDetail.js
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { ReactComponent as KakaoFill } from '../image/kakaofill.svg';
 
 const SDetailLayout = styled.main`
   padding: 24px;
+  min-height: calc(100vh - 60px - 280px);
+
+  .container {
+    max-width: 1280px;
+    margin: 0 auto;
+  }
 `;
 
 const SDetailWrap = styled.div`
@@ -11,16 +18,15 @@ const SDetailWrap = styled.div`
   gap: 35px;
 
   img {
-    margin: 24px;
+    margin: 24px 24px 24px 0px;
     width: 350px;
-    flex-shrink: 0;
   }
 `;
 
 const SRightSide = styled.div`
   margin: 24px;
-  min-width: calc(100% - 700px);
   /* border: 1px solid green; */
+  width: calc(100% - 610px);
 
   .controlButtons {
     flex-shrink: 0;
@@ -37,10 +43,16 @@ const SRightSide = styled.div`
       cursor: pointer;
     }
   }
+
+  .description {
+    font-size: 1.05rem;
+  }
 `;
 
 const STopWrap = styled.div`
+  padding-bottom: 5px;
   border-bottom: 1px solid #aaaaaa;
+  /* max-width: 677px; */
 
   h1 {
     margin: 10px 0px;
@@ -51,7 +63,6 @@ const STopWrap = styled.div`
     align-items: center;
     justify-content: space-between;
     gap: 40px;
-    max-width: 677px;
   }
 
   .authorInfo {
@@ -59,6 +70,7 @@ const STopWrap = styled.div`
     align-items: center;
     gap: 10px;
     margin-bottom: 10px;
+    padding-left: 3px;
 
     img {
       margin: 0;
@@ -74,94 +86,123 @@ const STopWrap = styled.div`
 `;
 
 const SBookInfo = styled.div`
-  margin-top: 10px;
+  margin-top: 20px;
+  margin-bottom: 40px;
+  /* border: 1px solid #bb2649; */
+  background-color: #fffbeac2;
   border-radius: 5px;
-  border-bottom: 1px solid #aaaaaa;
-  padding-bottom: 10px;
-  margin-bottom: 10px;
+  /* border-bottom: 1px solid #aaaaaa; */
+  padding: 24px 12px 5px 24px;
+
+  h2 {
+    margin-top: 0;
+  }
 
   div {
     color: #000000;
     margin: 5px 0px;
     font-weight: 600;
-    font-size: 1.1rem;
+    font-size: 1rem;
     span {
-      color: #7c7c7c;
+      color: #505050;
     }
   }
 `;
 
 const SContact = styled.div`
   display: flex;
+  justify-content: flex-end;
   align-items: center;
+
+  div {
+    font-weight: 600;
+    margin-right: 15px;
+    color: #7c7c7c;
+    font-size: 0.9rem;
+  }
 
   button {
     border: none;
-    border-radius: 70%;
+    border-radius: 5px;
     background-color: #f9e000;
-    padding: 7px;
+    padding: 12px;
     margin: 10px 5px 10px 0px;
+    font-weight: 600;
+    font-size: 0.9rem;
 
     display: flex;
     justify-content: center;
     align-items: center;
+    gap: 5px;
+    flex-shrink: 0;
   }
 `;
 
 const ReqDetail = () => {
   // 제목, 저자, 출판사, 오픈채팅, 본문, 작성자 닉네임
   const dummyData = {
-    title: '책 찾습니다',
+    title: '나눔해주실 분 찾아요',
     bookTitle: '모던 자바스크립트 Deep Dive',
     bookAuthor: '이웅모',
     bookPublisher: '위키북스',
     imgUrl:
       'https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9791158392239.jpg',
-    content: '갖고 계신 분 연락 부탁드립니다!',
+    content: '안 보시는 분 나눔 부탁드립니다 ㅠㅠ!',
   };
 
   return (
     <SDetailLayout>
-      <SDetailWrap>
-        <img alt="책 표지" src={dummyData.imgUrl} />
-        <SRightSide>
-          <STopWrap>
-            <div className="titleAndButton">
-              <h1>{dummyData.title}</h1>
-              {/* 수정, 삭제 버튼은 자기가 쓴 글에서만 보이도록 */}
-              <div className="controlButtons">
-                <span className="editButton">수정</span>
-                <span className="betweenButtons">|</span>
-                <span clasSName="deleteButton">삭제</span>
+      <div className="container">
+        <SDetailWrap>
+          <div>
+            <img alt="책 표지" src={dummyData.imgUrl} />
+          </div>
+          <SRightSide>
+            <STopWrap>
+              <div className="titleAndButton">
+                <h1>{dummyData.title}</h1>
+                {/* 수정, 삭제 버튼은 자기가 쓴 글에서만 보이도록 */}
+                <div className="controlButtons">
+                  <Link to="/reqEdit">
+                    <span className="editButton">수정</span>
+                  </Link>
+                  <span className="betweenButtons">|</span>
+                  <span clasSName="deleteButton">삭제</span>
+                </div>
               </div>
-            </div>
-            <div className="authorInfo">
-              <img
-                alt="profileImage"
-                src="https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/309/59932b0eb046f9fa3e063b8875032edd_crop.jpeg"
-              />
-              <div>닉네임</div>
-              <div className="createdAt">작성 날짜 회색으로</div>
-            </div>
-          </STopWrap>
-          <SBookInfo>
-            <h2>{dummyData.bookTitle}</h2>
-            <div>
-              저자: <span>{dummyData.bookAuthor}</span>
-            </div>
-            <div>
-              출판사: <span>{dummyData.bookPublisher}</span>
-            </div>
-          </SBookInfo>
-          <SContact>
-            <button>
-              <KakaoFill width="14" height="14" />
-            </button>
-            오픈채팅으로 연락하기
-          </SContact>
-          <div className="description">{dummyData.content}</div>
-        </SRightSide>
-      </SDetailWrap>
+              <div className="authorInfo">
+                <img
+                  alt="profileImage"
+                  src="https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/309/59932b0eb046f9fa3e063b8875032edd_crop.jpeg"
+                />
+                <div>닉네임</div>
+                <div className="createdAt">작성 날짜 회색으로</div>
+              </div>
+            </STopWrap>
+            <SBookInfo>
+              <h2>{dummyData.bookTitle}</h2>
+              <div>
+                저자: <span>{dummyData.bookAuthor}</span>
+              </div>
+              <div>
+                출판사: <span>{dummyData.bookPublisher}</span>
+              </div>
+              <SContact>
+                <div>이 책을 갖고 계신가요?</div>
+                <button
+                  onClick={() => {
+                    window.open('http://www.naver.com');
+                  }}
+                >
+                  <KakaoFill width="16" height="16" />
+                  오픈채팅으로 연락하기
+                </button>
+              </SContact>
+            </SBookInfo>
+            <p className="description">{dummyData.content}</p>
+          </SRightSide>
+        </SDetailWrap>
+      </div>
     </SDetailLayout>
   );
 };
