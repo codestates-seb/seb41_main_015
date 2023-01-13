046c9c1034234504bc68edd28c084f73921cfc96
djwjfk <grw07236@naver.com>
2023-01-13 17:22:13 +0900

    feat/fe/mypageEdit: 비동기처리

--- client/src/page/MyPageEdit.js
+++ client/src/page/MyPageEdit.js
@@ -2,10 +2,16 @@ import styled from 'styled-components';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
+const SWrapEdit = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 const SInputList = styled.div`
-  display: block;
-  text-align: center;
+  margin: 0 5%;
+
   p {
     margin-bottom: 31px;
   }
@@ -36,7 +42,9 @@ const SWithdraw = styled.div`
   line-height: 18px;
   color: #bb2649;
   cursor: pointer;
-  margin-left: 1100px;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 10%;
 `;
 
 const SCancelBtn = styled.button`
@@ -71,15 +79,15 @@ const SSaveBtn = styled.button`
 
 const SEditBtn = styled.div`
   text-align: center;
-  margin-top: 60px;
+  margin-bottom: 30%;
+  margin-top: 20px;
 `;
 
 const STitle = styled.h1`
-  margin-left: 54px;
+  margin-left: 5%;
 `;
 
 const SDefaultProfile = styled.div`
-  margin-left: 48%;
   img {
     height: 100px;
     width: 100px;
@@ -90,67 +98,68 @@ const SDefaultProfile = styled.div`
 const MyPageEdit = () => {
   //컴포넌트에서 바뀌는 값 관리
   const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-  const [email, setEmail] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  // const [email, setEmail] = useState('');
   const [address, setAddress] = useState('');
-  const [phonNumber, setPhonNumber] = useState('');
-  const [profile, setProfile] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  // const [profile, setProfile] = useState('');
 
   //현재 접속한 페이지의 id값을 가져옴
-  const { id } = useParams();
-  //navigate
+  // const { id } = useParams();
+
+  //경로 이동
   const navigate = useNavigate();
 
   //input 입력값 상태 저장
   const handleChangeName = (e) => {
     setName(e.target.value);
-    console.log(e.target.value);
   };
-  const handleChangeNickname = (e) => {
-    setNickname(e.target.value);
-    console.log(e.target.value);
+  const handleChangeDisplayName = (e) => {
+    setDisplayName(e.target.value);
   };
-  const handleChangeEmail = (e) => {
-    setEmail(e.target.value);
-    console.log(e.target.value);
-  };
+  // const handleChangeEmail = (e) => {
+  //   setEmail(e.target.value);
+  // };
   const handleChangeAddress = (e) => {
     setAddress(e.target.value);
-    console.log(e.target.value);
   };
-  const handleChangePhonNumber = (e) => {
-    setPhonNumber(e.target.value);
-    console.log(e.target.value);
+  const handleChangePhoneNumber = (e) => {
+    setPhoneNumber(e.target.value);
   };
-  const handleChangeProfile = (e) => {
-    setProfile(e.target.value);
-    console.log(e.target.value);
-  };
+  // const handleChangeProfile = (e) => {
+  //   setProfile(e.target.value);
+  // };
 
   //base url
   const url = 'https://serverbookvillage.kro.kr';
+
+  //임시 토큰
+  // const token =
+  //   'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJVU0VSIl0sInVzZXJuYW1lIjoiZ3J3MDcyMzY3MEBnbWFpbC5jb20iLCJzdWIiOiJncncwNzIzNjcwQGdtYWlsLmNvbSIsImlhdCI6MTY3MzU5MDY4MiwiZXhwIjoxNjczNTkyNDgyfQ.MGn55U1rZv5_pZCLoxkAdfL-aRnSUnvuijZVcFXxsfc';
+
+  //전역상태값 가져오기
+  const user = useSelector((state) => state.user);
+  console.log(user);
+
   //저장 버튼 클릭 시, 서버로 patch 요청
   const handleClickSave = () => {
     axios
       .patch(
-        url + `/v1/members`,
+        `${url}/v1/members`,
         {
           name,
-          nickname,
-          email,
+          displayName,
           address,
-          phonNumber,
-          profile,
+          phoneNumber,
+        },
+        // 토큰 부분
+        {
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            Accept: 'application/json',
+            Authorization: `Bearer ${user.accessToken}`,
+          },
         }
-        //토큰 부분
-        // {
-        //   headers: {
-        //     'Content-Type': 'application/json;charset=UTF-8',
-        //     Accept: 'application/json',
-        //     Authorization:
-        //       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJVU0VSIl0sInVzZXJuYW1lIjoidGpkdG40NDg0QGdtYWlsLmNvbSIsInN1YiI6InRqZHRuNDQ4NEBnbWFpbC5jb20iLCJpYXQiOjE2NzM1MTIzNjcsImV4cCI6MTY3MzUxNDE2N30.XcstPpk7smmT-_qAkeoo0jHQObjbrb9xEFt0XO5kxtQ ',
-        //   },
-        // }
       )
       .then(() => {
         navigate('/mypage');
@@ -165,114 +174,136 @@ const MyPageEdit = () => {
 
   // 서버 연결 후 주석 풀기!
   useEffect(() => {
-    axios
-      .get(url + `/v1/members`, {
-        //토큰 부분
-        // headers: {
-        //   'Content-Type': 'application/json;charset=UTF-8',
-        //   Accept: 'application/json',
-        //   Authorization:
-        //     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJVU0VSIl0sInVzZXJuYW1lIjoidGpkdG40NDg0QGdtYWlsLmNvbSIsInN1YiI6InRqZHRuNDQ4NEBnbWFpbC5jb20iLCJpYXQiOjE2NzM1MTIzNjcsImV4cCI6MTY3MzUxNDE2N30.XcstPpk7smmT-_qAkeoo0jHQObjbrb9xEFt0XO5kxtQ',
-        // },
-      })
-      .then((res) => {
+    const editData = async () => {
+      try {
+        const res = await axios.get(url + '/v1/members', {
+          // 토큰 부분
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            Accept: 'application/json',
+            Authorization: `Bearer ${user.accessToken}`,
+          },
+        });
         setName(res.data.name);
-        setNickname(res.data.nickname);
-        setEmail(res.data.email);
+        setDisplayName(res.data.displayName);
         setAddress(res.data.address);
-        setPhonNumber(res.data.phonNumber);
-        setPhonNumber(res.data.profile);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+        setPhoneNumber(res.data.phoneNumber);
+      } catch (error) {
+        console.error(error);
+        alert('정보를 불러오는데 실패했습니다');
+      }
+    };
+    editData();
   }, []);
+  //   axios
+  //     .get(url + '/v1/members', {
+  //       // 토큰 부분
+  //       headers: {
+  //         'Content-Type': 'application/json;charset=UTF-8',
+  //         Accept: 'application/json',
+  //         Authorization: `Bearer ${user.accessToken}`,
+  //       },
+  //     })
+  //     .then((res) => {
+  //       setName(res.data.name);
+  //       setDisplayName(res.data.displayName);
+  //       setEmail(res.data.email);
+  //       setAddress(res.data.address);
+  //       setPhoneNumber(res.data.phoneNumber);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //       alert('회원정보를 불러오는데 실패했습니다');
+  //     });
+  // }, []);
 
   return (
     <div>
       <STitle>회원정보 수정</STitle>
       <SHr />
-      <SDefaultProfile>
-        <img
-          src="https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/309/59932b0eb046f9fa3e063b8875032edd_crop.jpeg"
-          alt="profile"
-        />
-      </SDefaultProfile>
-      <SInputList>
-        <p>
-          <label htmlFor="name" className="mr-30">
-            이름:
-          </label>
-          <input
-            id="name"
-            className="inputSize"
-            placeholder="이름을 입력하십시오"
-            // value={name}
-            onChange={handleChangeName}
-          ></input>
-        </p>
-        <p>
-          <label htmlFor="nickName" className="mr-18">
-            닉네임:
-          </label>
-          <input
-            id="nickName"
-            className="inputSize"
-            placeholder="닉네임을 입력하십시오"
-            // value={nickname}
-            onChange={handleChangeNickname}
-          ></input>
-        </p>
-        <p>
-          <label htmlFor="email" className="mr-18">
-            이메일:
-          </label>
-          <input
-            id="email"
-            className="inputSize"
-            placeholder="이메일을 입력하십시오"
-            // value={email}
-            onChange={handleChangeEmail}
-          ></input>
-        </p>
-        <p>
-          <label htmlFor="address" className="mr-30">
-            주소:
-          </label>
-          <input
-            id="address"
-            className="inputSize"
-            placeholder="주소를 입력하십시오"
-            // value={address}
-            onChange={handleChangeAddress}
-          ></input>
-        </p>
-        <p>
-          <label htmlFor="phonNumber" className="mr-6">
-            전화번호:
-          </label>
-          <input
-            id="phonNumber"
-            className="inputSize"
-            placeholder="전화번호를 입력하십시오"
-            // value={phonNumber}
-            onChange={handleChangePhonNumber}
-          ></input>
-        </p>
-        <p>
-          <label htmlFor="profile" className="mr-18">
-            프로필:
-          </label>
-          <input
-            id="profile"
-            className="inputSize"
-            placeholder="프로필을 url형태로 입력하십시오"
-            // value={profile}
-            onChange={handleChangeProfile}
-          ></input>
-        </p>
-      </SInputList>
-      <SWithdraw>회원탈퇴</SWithdraw>
+      <SWrapEdit>
+        <SDefaultProfile>
+          <img
+            src="https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/309/59932b0eb046f9fa3e063b8875032edd_crop.jpeg"
+            alt="profile"
+          />
+        </SDefaultProfile>
+        <SInputList>
+          <p>
+            <label htmlFor="name" className="mr-30">
+              이름
+            </label>
+            <input
+              id="name"
+              className="inputSize"
+              placeholder="이름을 입력하십시오"
+              value={name}
+              onChange={handleChangeName}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor="nickName" className="mr-18">
+              닉네임
+            </label>
+            <input
+              id="nickName"
+              className="inputSize"
+              placeholder="닉네임을 입력하십시오"
+              value={displayName}
+              onChange={handleChangeDisplayName}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor="email" className="mr-18">
+              이메일
+            </label>
+            <input
+              id="email"
+              className="inputSize"
+              placeholder="이메일을 입력하십시오"
+              // value={email}
+              // onChange={handleChangeEmail}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor="address" className="mr-30">
+              주소
+            </label>
+            <input
+              id="address"
+              className="inputSize"
+              placeholder="주소를 입력하십시오"
+              value={address}
+              onChange={handleChangeAddress}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor="phonNumber" className="mr-6">
+              전화번호
+            </label>
+            <input
+              id="phonNumber"
+              className="inputSize"
+              placeholder="전화번호를 입력하십시오"
+              value={phoneNumber}
+              onChange={handleChangePhoneNumber}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor="profile" className="mr-18">
+              프로필
+            </label>
+            <input
+              id="profile"
+              className="inputSize"
+              placeholder="프로필을 url형태로 입력하십시오"
+              // value={profile}
+              // onChange={handleChangeProfile}
+            ></input>
+          </p>
+          <SWithdraw>회원탈퇴</SWithdraw>
+        </SInputList>
+      </SWrapEdit>
       <SEditBtn>
         <SCancelBtn>취소</SCancelBtn>
         <SSaveBtn onClick={handleClickSave}>저장</SSaveBtn>
