9e2daf4e0ecd522e91b03e3cd515e698467eca4f
hi <111436967+cjsrmachs@users.noreply.github.com>
2023-01-16 11:44:21 +0900

    nothing

--- client/src/page/MyPage.js
+++ client/src/page/MyPage.js
@@ -1,124 +1,262 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import dummy from "../data/data.json";
-import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
 
+const STable = styled.table`
+margin-left:auto;
+margin-right:auto;
+
+
+
+`;
+
+const SOverflow = styled.div`
+overflow-y:auto;
+height:700px;
+`;
 
 const SBoxA = styled.div`
-  overflow:scroll;
-height:300px
+overflow: scroll;
+::-webkit-scrollbar{width: 4px;}
+::-webkit-scrollbar-thumb{background-color: #bb2649; border-radius: 10px;}
+height:300px;
 `;
+
 const SBoxB = styled.div`
 overflow:scroll;
+::-webkit-scrollbar{width: 4px;}
+::-webkit-scrollbar-thumb{background-color: #bb2649; border-radius: 10px;}
 height:120px
-
 `;
+
 const SBoxC = styled.div`
 overflow:scroll;
+::-webkit-scrollbar{width: 4px;}
+::-webkit-scrollbar-thumb{background-color: #bb2649; border-radius: 10px;}
 height:120px
 `;
+
 const SProfile = styled.td`
-  width:30%;
+font-size: 14px;
+font-weight: 600;
+transform: translateX(50px);
+
+`;
+
+const SDivide = styled.div`
+border : 1px solid black;
+
 `;
 
 const SInformation = styled.button`
-  width: 90px;
+width: 90px;
   height: 33px;
   font-size: 16px;
   font-weight: 600;
   color: #bb2649;
   border: 1px solid #bb2649;
-  border-radius: 20px;
+  border-radius: 10px;
   :hover {
     color: #ffffff;
     background-color: #bb2649;
   }
+  margin-right:54px;
 `;
-const SText = styled.h4`
-  width: 90px;
-  height: 33px;
-  font-size: 16px;
-  font-weight: 600;
-  color: #bb2649;
-  right:30px;
-  text-align:center;
+
+
+const SInformationtd = styled.td`
+text-align:right;
+width:50%;
 `;
+const SMyPageText = styled.h3`
+  width: 200px;
+  height: 10px;
+  font-weight: 750;
+  font-size: 30px;
+  text-align:right;
+`;
+const SText = styled.td`
+  font-size: 13px;
+`;
+const SCommunity = styled.div`
+border-bottom:1px solid black;
+`;
+const SComment = styled.strong`
+background : lightgray;
+`;
+
+const SHR = styled.hr`
+margin-bottom: 50px;
+`;
+
+const SImage = styled.img`
+border-radius:50%;
+width:150px;
+height:150px;
+margin-left:50px;
+
+`;
+
+const SDivideImage = styled.img`
+width:70%;
+display:flex;
+`;
+
+const STitle = styled.h1`
+  margin-left: 54px;
+  height: 30px;
+  font-size: 30px;
+  font-weight: 730;
+`;
+
+const STitle2 = styled.h2`
+  margin-left: 10px;
+  font-size: 20px;
+ 
+`;
+
+const STextStrong = styled.strong`
+  font-size: 18px;
+`;
+
+
 const MyPage = () => {
   const books = dummy.books;
   const comments = dummy.comment;
   const community = dummy.community;
-  console.log(comments);
+  const navigate = useNavigate();
+  // const [books,setBooks] = useState();
+  // const [comments,setComments] = useState();
+  // const [community,setcommunity] = useState();
+  // useEffect(()=>{
+  //   axios.get("http://localhost~")
+  //   .then(() => {
+  //     ;
+  //   })
+  //   .catch((err) => {
+  //     alert('데이터가 불러오기 실패');
+  //     console.error(err);
+  //   });
+
+  //   axios.get("http://localhost~")
+  //   .then(() => {
+  //    ;
+  //   })
+  //   .catch((err) => {
+  //     alert('데이터가 불러오기 실패');
+  //     console.error(err);
+  //   });
+
+  //   axios.get("http://localhost~")
+  //   .then(() => {
+  //     ;
+  //   })
+  //   .catch((err) => {
+  //     alert('데이터가 불러오기 실패');
+  //     console.error(err);
+  //   });
+  // })
+
   return <>
-    <table style={{ width: "80%", height: "100%"}}>
+    <SOverflow>
+      <STable>
+        <tbody>
+          <tr>
+            <td><STitle>마이 페이지</STitle> </td>
+            <SInformationtd><SInformation onClick={() => navigate('/mypageEdit')}>정보수정</SInformation></SInformationtd>
+          </tr>
+          <tr>
+            <td colSpan={2}>
+              <SHR></SHR>
+            </td>
+          </tr>
     
-      <tbody>
-        <tr>
-          <td><SText>마이 페이지</SText> </td>
-          <td></td>
-          <td style={{textAlign:"center"}}><SInformation>정보수정</SInformation></td>
-        </tr>
-       <tr>
-       <td colSpan={3}> <hr></hr></td>
-       </tr>
-        <tr>
-          <td  colSpan={1} style={{textAlign:"center"}}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQve-F1mablIoZC6aIu2aVyBwDsaCAUu2863A&usqp=CAU" style={{borderRadius:"50%"}} alt="..."></img>
-          </td>
-          <td >
-            이메일 : seb41mainporject@naver.com
-            <br></br>
-            닉네임 : Groot
-          </td>
-          <td></td>
-        </tr>
-  
-        <tr>
-          <td>
-            <h5>나눔 중</h5>
-            <SBoxA>
-              {
-                books.map((book) => (
-                  <div style={{border:"1px solid black"}}>
-                    <tr>제목 : {book.name}</tr>
-                    <tr>순서 : {book.id}</tr>
-                    <tr>작성자 : {book.author}</tr>
-                  </div>
-                ))
-              }
-            </SBoxA>
-          </td>
-          <td>
-            <h5>커뮤니티 활동</h5>
+          <tr>
+            <td>
+              <tr>
+                <td><SImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQve-F1mablIoZC6aIu2aVyBwDsaCAUu2863A&usqp=CAU" alt="..."></SImage></td>
+                <SProfile>
+                  이메일
+                  <br></br>
+                  seb41mainporject@naver.com
+                  <br></br>
+                  <br></br>
+                  닉네임
+                  <br></br>
+                  Groot
+                </SProfile>
+              </tr>
+            </td>
+            <td>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <STitle2>나눔 중</STitle2>
+              <SBoxA>
+                {
+                  books.map((book) => (
+                    <>
+                      <SDivide>
+                        <table>
+                          <tr>
+                            <td><SDivideImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9sYgypcA8MhjHyNJD7-u9L0EjZj7YomKVTA&usqp=CAU" alt="..."></SDivideImage></td>
+                            <td>
+                              <tr>
+                                <td><STextStrong>{book.name}</STextStrong></td>
+                              </tr>
+                              <tr>
+                                <td><strong>책 제목</strong> : {book.name}</td>
+                              </tr>
+                              <tr>
+                                <SText>저자 : {book.author}, 출판사 : {book.author}</SText>
+                              </tr>
+                              <tr>
+                                <SText>작성일자 : {book.id}</SText>
+                              </tr>
+                            </td>
+                          </tr>
+                        </table>
 
-            <SBoxB>
-              {
-                community.map((communities) => (
-                  <div class="border-bottom border-dark">
-                    <tr>제목 : {communities.name} </tr>
-                    <tr>북클럽이름 : {communities.bookclub} </tr>
-                    <tr>책 제목 : {communities.bookname} </tr>
-                  </div>
-                ))
-              }
-            </SBoxB>
-
-            <h5>댓글</h5>
-            <SBoxC>
-              {
-                comments.map((comment) => (
-                  <>
-                    <tr className='bg-secondary bg-opacity-50'><strong>{comment.id} : {comment.comment}</strong></tr>
-                  </>
-                ))
-              }
-            </SBoxC>
-          </td>
-        </tr>
-       
-
-       
-      </tbody>
-
-    </table>
+                      </SDivide>
+                      <br></br>
+                    </>
+                  ))
+                }
+              </SBoxA>
+            </td>
+            <td>
+              <STitle2>커뮤니티 활동</STitle2>
+              <SBoxB>
+                {
+                  community.map((communities) => (
+                    <SCommunity>
+                      <tr>제목 : {communities.name} </tr>
+                      <tr>북클럽이름 : {communities.bookclub} </tr>
+                      <tr>책 제목 : {communities.bookname} </tr>
+                      <br></br>
+                    </SCommunity>
+                  ))
+                }
+              </SBoxB>
+              <STitle2>댓글</STitle2>
+              <SBoxC>
+                {
+                  comments.map((comment) => (
+                    <>
+                      <tr><SComment>{comment.id} : {comment.comment}</SComment></tr>
+                      <br></br>
+                    </>
+                  ))
+                }
+              </SBoxC>
+            </td>
+          </tr>
+        </tbody>
+      </STable>
+    </SOverflow>
   </>;
 };
 export default MyPage;
