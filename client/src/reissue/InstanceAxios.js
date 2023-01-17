cb42f0508f4b3d53a8b22e4e65df4f5ebb15515c
djwjfk <grw07236@naver.com>
2023-01-17 17:22:23 +0900

    fix/fe/refreshToken: 갱신 실패

--- client/src/reissue/InstanceAxios.js
+++ client/src/reissue/InstanceAxios.js
@@ -16,6 +16,7 @@ instanceAxios.interceptors.request.use(
     // 요청이 전달되기 전에 작업 수행
     const sessionAccessToken = sessionStorage.getItem('accessToken'); // access 토큰을 가져오는 함수
     if (sessionAccessToken) {
+      console.log('sessionAccessToken:', sessionAccessToken); //=?!!!! 엑세스토큰이 만료되면 여기가 undefined로 들어오고 있다...
       config.headers['Authorization'] = `Bearer ${sessionAccessToken}`;
     }
     return config;
@@ -28,40 +29,89 @@ instanceAxios.interceptors.request.use(
 
 //###3. 응답 인터셉터 추가하기
 instanceAxios.interceptors.response.use(
-  (response) => {
+  function (response) {
     //응답데이터 가공
+    console.log('get response', response); //res.config.headers.Authorization === Bearer {엑세스토큰}
     return response;
   },
-  async (error) => {
-    //오류응답 처리
-    const { response: errorResponse } = error; //구조분해할당
+  async function (error) {
+    // 오류 응답처리
+    const { response: errorResponse } = error;
+    const originalRequest = error.config;
+
     // 인증 에러 발생시
     if (errorResponse.status === 401) {
-      const originalRequest = errorResponse.config; //response.config
-      const sessionRefreshToken = sessionStorage.getItem('refreshToken');
-      const { data } = await axios.post(
-        'https://serverbookvillage.kro.kr/auth/token',
-        {},
-        {
-          headers: {
-            Authorization: { sessionRefreshToken },
-          },
-        }
-      );
-      const {
-        Authorization: newCreatedAccessToken,
-        // refreshToken: newRefreshToken,
-      } = data;
-
-      sessionStorage.setItem('accessToken', newCreatedAccessToken);
-
-      originalRequest.headers[
-        'Authorization'
-      ] = `Bearer ${newCreatedAccessToken}`;
-      return axios(originalRequest);
+      return await resetTokenAndReattemptRequest(error);
     }
+
     return Promise.reject(error);
   }
 );
+let isAlreadyFetchingAccessToken = false;
+let subscribers = [];
+
+async function resetTokenAndReattemptRequest(error) {
+  console.log('error:', error);
+  try {
+    const { response: errorResponse } = error;
+
+    // subscribers에 access token을 받은 이후 재요청할 함수 추가 (401로 실패했던)
+    // retryOriginalRequest는 pending 상태로 있다가
+    // access token을 받은 이후 onAccessTokenFetched가 호출될 때
+    // access token을 넘겨 다시 axios로 요청하고
+    // 결과값을 처음 요청했던 promise의 resolve로 settle시킨다.
+    const retryOriginalRequest = new Promise((resolve, reject) => {
+      addSubscriber(async (accessToken) => {
+        console.log('accessToken', accessToken);
+        try {
+          errorResponse.config.headers['Authorization'] =
+            'Bearer ' + accessToken;
+          resolve(instanceAxios(errorResponse.config));
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+
+    // refresh token을 이용해서 access token 요청
+    if (!isAlreadyFetchingAccessToken) {
+      isAlreadyFetchingAccessToken = true; // 문닫기 (한 번만 요청)
+      const sessionRefreshToken = sessionStorage.getItem('refreshToken');
+      const { data } = await axios.post(
+        'https://serverbookvillage.kro.kr/auth/token',
+        {
+          headers: {
+            Authorization: sessionRefreshToken,
+          },
+        }
+      );
+
+      isAlreadyFetchingAccessToken = false; // 문열기 (초기화)
+
+      onAccessTokenFetched(data.headers.get('Authorization'));
+    }
+
+    return retryOriginalRequest; // pending 됐다가 onAccessTokenFetched가 호출될 때 resolve
+  } catch (error) {
+    // signOut();
+    return Promise.reject(error);
+  }
+}
+
+function addSubscriber(callback) {
+  subscribers.push(callback);
+}
+console.log('subscribers', subscribers);
+
+function onAccessTokenFetched(accessToken) {
+  subscribers.forEach((callback) => callback(accessToken));
+  subscribers = [];
+}
+
+// function signOut() {
+//   removeUserToken('access');
+//   removeUserToken('refresh');
+//   window.location.href = '/signin';
+// }
 
 export default instanceAxios;
