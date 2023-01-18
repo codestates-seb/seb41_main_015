93333c09072033e74a2e5afb41eb6cc7b6f62410
jonghyuk <pjongk148@gmail.com>
2023-01-18 16:00:04 +0900

    hotfix/be/req - request commit

--- server/src/main/java/com/book/village/server/domain/request/controller/RequestController.java
+++ server/src/main/java/com/book/village/server/domain/request/controller/RequestController.java
@@ -60,6 +60,7 @@ public class RequestController {
                 HttpStatus.OK);
     }
 
+
     @GetMapping("/mine")
     public ResponseEntity getMyRequests(@PageableDefault Pageable pageable, Principal principal) {
         Page<Request> myRequests = requestService.findMyRequests(principal.getName(),pageable);
