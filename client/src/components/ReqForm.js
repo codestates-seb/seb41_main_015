7e66e5832925c992fe8d40230ddf96a9009dacdd
yejin32 <vhdekddl3@naver.com>
2023-01-19 18:10:27 +0900

    feat/fe/ReqAdd: 요청 등록 구현

---
+++ client/src/components/ReqForm.js
@@ -0,0 +1,212 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import BookAddModal from './BookAddModal';
+
+const StyledReqForm = styled.div`
+  h2 {
+    color: #2c2c2c;
+    padding: 18px;
+    margin: 20px 10%;
+    border-bottom: 1px solid #acacac;
+  }
+`;
+const SInputContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 30px 11% 10px 14%;
+  font-size: 13px;
+  #ImgInput {
+    display: none;
+  }
+  @media screen and (max-width: 930px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0;
+  }
+`;
+
+const SInputLeft = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SImgBtn = styled.div`
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  margin-bottom: 20px;
+  .bookImg {
+    /* border: 1px solid #000000; */
+    width: 230px;
+    height: 297.156px;
+    margin-bottom: 30px;
+    @media screen and (max-width: 930px) {
+      width: 200px;
+    }
+  }
+`;
+
+const SInputRight = styled.div`
+  width: 65%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+  input {
+    width: 95%;
+    height: 40px;
+    margin-bottom: 20px;
+    border: 1px solid #aaaaaa;
+    border-radius: 4px;
+    padding-left: 10px;
+  }
+  .inputContent {
+    width: 95%;
+    height: 150px;
+    border: 1px solid #aaaaaa;
+    border-radius: 4px;
+    margin-bottom: 30px;
+    padding: 10px;
+  }
+`;
+
+const SButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 70px;
+  button {
+    width: 15%;
+    height: 35px;
+    border: 1px solid #bb2649;
+    border-radius: 5px;
+  }
+  .cancelBtn {
+    color: #bb2649;
+    background-color: #ffffff;
+    border: 1px solid #bb2649;
+    margin-right: 5%;
+    :hover {
+    }
+  }
+  .submitBtn {
+    color: #ffffff;
+    background-color: #bb2649;
+    :hover {
+    }
+  }
+`;
+
+const ReqForm = (props) => {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const { inputs, onBookInfoChange } = props;
+  const { bookTitle, author, publisher, talkUrl, title, content, thumbnail } =
+    inputs;
+
+  const handleChangeString = (e, type) => {
+    onBookInfoChange({ ...inputs, [`${type}`]: e.target.value });
+    console.log(e.target.value);
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <StyledReqForm>
+      <h2>{props.page === 'reqAdd' ? '요청하기' : '수정하기'}</h2>
+      <SInputContainer>
+        <SInputLeft>
+          <SImgBtn>
+            <img
+              alt="bookImg"
+              src={thumbnail}
+              className="bookImg"
+              onChange={(e) => handleChangeString(e, thumbnail)}
+            />
+          </SImgBtn>
+        </SInputLeft>
+        <SInputRight>
+          <div>
+            <input
+              name="bookTitle"
+              value={bookTitle || ''}
+              onChange={(e) => handleChangeString(e, bookTitle)}
+              placeholder="책 제목을 입력해주세요."
+              onClick={handleOpenModal}
+            />
+            {/* 검색 모달  */}
+            <BookAddModal
+              isModalOpen={isModalOpen}
+              onBookInfoChange={onBookInfoChange}
+              handleCloseModal={handleCloseModal}
+            />
+          </div>
+          <div>
+            <input
+              name="author"
+              value={author || ''}
+              onChange={(e) => handleChangeString(e, author)}
+              placeholder="저자를 입력해주세요."
+              disabled
+            />
+          </div>
+          <div>
+            <input
+              name="publisher"
+              value={publisher || ''}
+              onChange={(e) => handleChangeString(e, publisher)}
+              placeholder="출판사를 입력해주세요."
+              disabled
+            />
+          </div>
+          <div>
+            <input
+              name="talkUrl"
+              value={talkUrl || ''}
+              onChange={(e) => handleChangeString(e, 'talkUrl')}
+              placeholder="오픈채팅 대화방 링크를 입력해주세요."
+            />
+          </div>
+          <div>
+            <input
+              name="title"
+              value={title || ''}
+              onChange={(e) => handleChangeString(e, 'title')}
+              placeholder="게시글 제목을 입력해주세요."
+            />
+          </div>
+          <div>
+            <textarea
+              name="content"
+              value={content || ''}
+              onChange={(e) => handleChangeString(e, 'content')}
+              className="inputContent"
+              placeholder="게시글 내용을 입력해주세요. (ex. 책 상태, 구매 시기 등)"
+            />
+          </div>
+        </SInputRight>
+      </SInputContainer>
+      <SButtonBox>
+        <button className="cancelBtn" onClick={goBack}>
+          취소
+        </button>
+        <button className="submitBtn" onClick={props.editBtn}>
+          {props.page === 'reqAdd' ? '요청' : '수정'}
+        </button>
+      </SButtonBox>
+    </StyledReqForm>
+  );
+};
+
+export default ReqForm;
--- client/src/page/ReqAdd.js
+++ client/src/page/ReqAdd.js
@@ -1,4 +1,63 @@
+import ReqForm from '../components/ReqForm';
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+import instanceAxios from '../reissue/InstanceAxios';
+import { useNavigate } from 'react-router-dom';
+
 const ReqAdd = () => {
-  return <p>요청 등록 부분입니다</p>;
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({
+    bookTitle: '',
+    author: '',
+    publisher: '',
+    talkUrl: '',
+    title: '',
+    content: '',
+  });
+
+  const { bookTitle, author, publisher, talkUrl, title, content } = inputs;
+
+  const handleClickSubmit = () => {
+    console.log(inputs);
+    instanceAxios
+      .post('/v1/requests', {
+        bookTitle,
+        author,
+        publisher,
+        talkUrl,
+        title,
+        content,
+      })
+      .then((res) => {
+        console.log(res);
+        Swal.fire(
+          '요청 글 등록 완료.',
+          '요청 글이 정상적으로 작성되었습니다.',
+          'success'
+        );
+        navigate('/reqList');
+      })
+      .catch((err) => {
+        Swal.fire(
+          '요청글 작성 실패',
+          '글 작성이 완료되지 않았습니다.',
+          'warning'
+        );
+      });
+  };
+
+  const handleBookInfoChange = (bookInfo) => {
+    setInputs(bookInfo);
+  };
+
+  return (
+    <ReqForm
+      page="reqAdd"
+      editBtn={handleClickSubmit}
+      inputs={inputs}
+      onBookInfoChange={handleBookInfoChange}
+    ></ReqForm>
+  );
 };
 export default ReqAdd;
