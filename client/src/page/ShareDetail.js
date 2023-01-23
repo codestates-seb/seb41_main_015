d9ee699a77c999e8263efe18eb4da2354d825265
djwjfk <grw07236@naver.com>
2023-01-23 15:25:54 +0900

    fix/fe/comment: 공통 컴포넌트 사용

--- client/src/page/ShareDetail.js
+++ client/src/page/ShareDetail.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import DetailForm from '../components/DetailForm';
 import Swal from 'sweetalert2';
+import Comment from '../components/Comment';
 
 const ShareDetail = () => {
   const { id } = useParams();
@@ -15,6 +16,7 @@ const ShareDetail = () => {
       .get(url + `v1/borrows/${id}`)
       .then((res) => {
         setData(res.data.data);
+        console.log('getShareDetail', res.data.data);
       })
       .catch((err) => {
         Swal.fire('데이터 로딩 실패', '데이터 로딩에 실패했습니다.', 'warning');
@@ -25,6 +27,7 @@ const ShareDetail = () => {
   return (
     <>
       <DetailForm data={data} page="share" id={id} />
+      <Comment data={data} page="share" id={id} />
     </>
   );
 };
