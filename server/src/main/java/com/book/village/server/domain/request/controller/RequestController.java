a8892d4b670dab7a1185c7436e0daa24964f51d6
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-01-25 09:58:41 +0900

    Refactor/be/views (#183)
    * feat/be/rank - add request views & yml fix
    
    * fix/be/rank - test code fix
    
    * feat/be/rank - request rank 생성 및 request views field 추가
    
    * fix/be/rank - requestrank REST API DOCS 를 통한 문서화
    
    * fix/be/rank - 일부 코드 및 adoc 파일 수정
    
    * fix/be/rank - adoc 파일 수정

--- server/src/main/java/com/book/village/server/domain/request/controller/RequestController.java
+++ server/src/main/java/com/book/village/server/domain/request/controller/RequestController.java
@@ -1,8 +1,7 @@
 package com.book.village.server.domain.request.controller;
-import com.book.village.server.domain.community.dto.CommunityDto;
-import com.book.village.server.domain.community.entity.Community;
 import com.book.village.server.domain.request.dto.RequestDto;
 import com.book.village.server.domain.request.entity.Request;
+import com.book.village.server.domain.request.entity.RequestRank;
 import com.book.village.server.domain.request.mapper.RequestMapper;
 import com.book.village.server.domain.request.service.RequestService;
 import com.book.village.server.global.response.ListResponse;
@@ -93,4 +92,12 @@ public class RequestController {
         requestService.deleteRequest(requestId, principal.getName());
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
+
+    @GetMapping("/rank")
+    public ResponseEntity RequestRank() {
+        List<RequestRank> rankResponses = requestService.findRankedRequests();
+        return new ResponseEntity(
+                new ListResponse<>(requestMapper.requestRanksTorankedResponses(rankResponses)),HttpStatus.OK);
+    }
 }
+
