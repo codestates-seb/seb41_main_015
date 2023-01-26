93e0adbc4040a0450b2744080f2b575b526e964c
Changhoon <changhoon@Changhoons-MacBook-Pro.local>
2023-01-26 10:43:40 +0900

    g

--- client/src/page/MyPage.js
+++ client/src/page/MyPage.js
@@ -1,56 +1,66 @@
-import axios from 'axios';
+
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import dummy from "../data/data.json";
+
 import {useNavigate} from 'react-router-dom';
+import instanceAxios from '../reissue/InstanceAxios';
 
 const STable = styled.table`
-margin-left:auto;
-margin-right:auto;
+  /* margin-left: auto;
+  margin-right: auto; */
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
 
 `;
 
 const SOverflow = styled.div`
-overflow-y:auto;
-height:700px;
+  overflow-y: auto;
+  height: 700px;
 `;
 
 const SBoxA = styled.div`
-overflow: scroll;
-::-webkit-scrollbar{width: 4px;}
-::-webkit-scrollbar-thumb{background-color: #bb2649; border-radius: 10px;}
-height:300px;
-padding: 10px;
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    width: 4px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #bb2649;
+    border-radius: 10px;
+  }
+  height: 300px;
+  padding: 10px;
 `;
 
 const SBoxB = styled.div`
-overflow:scroll;
-::-webkit-scrollbar{width: 4px;}
-::-webkit-scrollbar-thumb{background-color: #bb2649; border-radius: 10px;}
-height:300px;
-padding: 10px;
-
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    width: 4px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #bb2649;
+    border-radius: 10px;
+  }
+  height: 300px;
+  padding: 10px;
 `;
 
-
-
 const SProfile = styled.td`
-font-size: 14px;
-font-weight: 600;
-transform: translateX(50px);
-
+  font-size: 14px;
+  font-weight: 600;
+  transform: translateX(50px);
 `;
 
 const SDivide = styled.div`
-border : 1px solid black;
-
+  border: 1px solid black;
 `;
 
 const SInformation = styled.button`
-width: 90px;
+  width: 90px;
   height: 33px;
   font-size: 16px;
   font-weight: 600;
+  float: right;
   color: #bb2649;
   border: 1px solid #bb2649;
   border-radius: 10px;
@@ -58,59 +68,58 @@ width: 90px;
     color: #ffffff;
     background-color: #bb2649;
   }
-  margin-right:54px;
+  margin-right: 10%;
 `;
 
-
 const SInformationtd = styled.td`
-text-align:right;
-width:50%;
+  text-align: right;
+  width: 50%;
 `;
 const SMyPageText = styled.h3`
   width: 200px;
   height: 10px;
   font-weight: 750;
   font-size: 30px;
-  text-align:right;
+  text-align: right;
 `;
 const SText = styled.td`
   font-size: 13px;
 `;
 const SCommunity = styled.div`
-border-bottom:1px solid black;
+  border-bottom: 1px solid black;
 `;
 const SComment = styled.strong`
-background : lightgray;
+  background: lightgray;
 `;
 
 const SHR = styled.hr`
-margin-bottom: 50px;
+  margin-bottom: 50px;
 `;
 
 const SImage = styled.img`
-border-radius:50%;
-width:150px;
-height:150px;
-margin-left:50px;
-
+  border-radius: 50%;
+  width: 150px;
+  height: 150px;
+  margin-left: 50px;
 `;
 
 const SDivideImage = styled.img`
-width:70%;
-display:flex;
+  width: 70%;
+  display: flex;
 `;
 
-const STitle = styled.h1`
-  margin-left: 54px;
-  height: 30px;
-  font-size: 30px;
-  font-weight: 730;
+const STitle = styled.div`
+  h2 {
+    color: #2c2c2c;
+    padding: 18px;
+    margin: 20px 10%;
+    border-bottom: 1px solid #acacac;
+  }
 `;
 
 const STitle2 = styled.h2`
   margin-left: 10px;
   font-size: 20px;
- 
 `;
 
 const STextStrong = styled.strong`
@@ -118,58 +127,50 @@ const STextStrong = styled.strong`
 `;
 
 
-
 const MyPage = () => {
-  const books = dummy.books;
-  const comments = dummy.comment;
-  const community = dummy.community;
-  const navigate = useNavigate();
-  const Nickname = "Groot"; 
-  // const [books,setBooks] = useState();
-  // const [comments,setComments] = useState();
-  // const [community,setcommunity] = useState();
-  // useEffect(()=>{
-  //   axios.get("http://localhost~")
-  //   .then(() => {
-  //     ;
-  //   })
-  //   .catch((err) => {
-  //     alert('데이터가 불러오기 실패');
-  //     console.error(err);
-  //   });
-
-  //   axios.get("http://localhost~")
-  //   .then(() => {
-  //    ;
-  //   })
-  //   .catch((err) => {
-  //     alert('데이터가 불러오기 실패');
-  //     console.error(err);
-  //   });
-
-  //   axios.get("http://localhost~")
-  //   .then(() => {
-  //     ;
-  //   })
-  //   .catch((err) => {
-  //     alert('데이터가 불러오기 실패');
-  //     console.error(err);
-  //   });
-  // })
+    const navigate = useNavigate();
+    const [email,setEmail] = useState('');
+    const [nickname,setNickname] = useState('');
+    const [data,setData] = useState([]);
+  
+    useEffect(()=>{
+       instanceAxios.get("/v1/members")
+      .then((res) => {
+        console.log(res)
+        setEmail(res.data.data.email);
+      setNickname(res.data.data.displayName);
+       ;
+      }) 
+    
+  
+      instanceAxios.get("/v1/borrows/mine?page=0&size=10&sort=createdAt%2Cdesc")
+      .then((res) => {
+        console.log(res)
+        setData(res.data.data)
+        ;
+      })
+  
+    })
 
   return <>
-    <SOverflow>
-      <STable>
-        <tbody>
-          <tr>
-            <td><STitle >마이 페이지</STitle> </td>
-            <SInformationtd><SInformation onClick={() => navigate('/mypageEdit')}>정보수정</SInformation></SInformationtd>
-          </tr>
-          <tr>
-            <td colSpan={2}>
-              <SHR></SHR>
-            </td>
-          </tr>
+      <SOverflow>
+        <STitle>
+          <h2>마이 페이지</h2>
+        </STitle>
+        <div>
+          <SInformation onClick={() => navigate('/mypageEdit')}>
+            정보수정
+          </SInformation>
+        </div>
+        <STable>
+          <tbody>
+            <tr>
+              <SInformationtd></SInformationtd>
+            </tr>
+            <tr>
+              <td colSpan={2}></td>
+            </tr>
+
     
           <tr>
             <td>
@@ -178,12 +179,12 @@ const MyPage = () => {
                 <SProfile>
                   이메일
                   <br></br>
-                  seb41mainporject@naver.com
+                  {email}
                   <br></br>
                   <br></br>
                   닉네임
                   <br></br>
-                  {Nickname}
+                  {nickname}
                 </SProfile>
               </tr>
             </td>
@@ -192,10 +193,10 @@ const MyPage = () => {
           </tr>
           <tr>
             <td>
-              <STitle2>{Nickname}의 나눔 세상</STitle2>
+              <STitle2>{nickname}의 나눔 세상</STitle2>
               <SBoxA>
-                {
-                  books.map((book) => (
+                {data.length !== 0 ? (
+                  data.map((book) => (
                     <>
                       <SDivide>
                         <table>
@@ -203,13 +204,14 @@ const MyPage = () => {
                             <td><SDivideImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9sYgypcA8MhjHyNJD7-u9L0EjZj7YomKVTA&usqp=CAU" alt="..."></SDivideImage></td>
                             <td>
                               <tr>
-                                <td><STextStrong>{book.name}</STextStrong></td>
+                                <td><STextStrong>{book.title}</STextStrong></td>
                               </tr>
                               <tr>
-                                <td><strong>책 제목</strong> : {book.name}</td>
+                                <td><strong>책 제목</strong> : {book.bookTitle}</td>
                               </tr>
                               <tr>
-                                <SText>저자 : {book.author}, 출판사 : {book.author}</SText>
+                                <SText>저자 : {book.author}</SText>
+                                <SText>출판사 : {book.author}</SText>
                               </tr>
                               <tr>
                                 <SText>작성일자 : {book.id}</SText>
@@ -222,22 +224,28 @@ const MyPage = () => {
                       <br></br>
                     </>
                   ))
-                }
+                ) : (
+                    <SDivide>
+                        <table>
+                            <tr>
+                                데이터가없습니다.
+                            </tr>
+                        </table>
+                    </SDivide>
+                )}
+                
               </SBoxA>
             </td>
             <td>
-              <STitle2>우리{Nickname}의 커뮤 활동 </STitle2>
+              <STitle2>우리{nickname}의 커뮤 활동 </STitle2>
               <SBoxB>
-                {
-                  community.map((communities) => (
-                    <SCommunity>
-                      <tr>제목 : {communities.name} </tr>
-                      <tr>북클럽이름 : {communities.bookclub} </tr>
-                      <tr>책 제목 : {communities.bookname} </tr>
-                      <br></br>
-                    </SCommunity>
-                  ))
-                }
+              <SDivide>
+                        <table>
+                            <tr>
+                                데이터가없습니다.
+                            </tr>
+                        </table>
+                </SDivide>
               </SBoxB>
              
             </td>
