20530b03e01137986e56dbaf2067a0fb3c95fcb1
yejin32 <vhdekddl3@naver.com>
2023-01-31 16:10:57 +0900

    fix/fe/client: 코드 수정

--- client/src/page/Callback.js
+++ client/src/page/Callback.js
@@ -22,7 +22,7 @@ const Callback = () => {
     // main page에서 accessToken이 존재하고 new일 때만 모달 띄우도록 하기
   }, []);
 
-  // git test
+  // 홈으로 이동
   useEffect(() => {
     navigate('/');
   }, [sessionAccessToken]);
